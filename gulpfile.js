0dd7063d45c884b867e14aaaa5464feb264e2dc3
gadarohan17 <gadarohan17@gmail.com>
2016-06-27 15:32:55 +0530

    JPP FTPString

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,9 @@ var jsArray = [
 var replacehostFrom = "http://localhost/demo/";
 var replacehostTo = "http://wohlig.co.in/demo2/";
 
-var ftpString = "U2FsdGVkX1+jcFED/CJbcYNiOJ42eBsjlxqmrcKWSIPH9Sao/4535zPQX5Fa7VYGAHSfkKCXbDpiUfJhkRRijaerS1lJ/k+dSfqsfl45ICkzMTJ7fBNVDj/242ur9ZG4HZDhSe1O/J4vEUboWDRBhg==";
+var ftpString = "U2FsdGVkX19fEFj78reyTrJyfkOEEctph92umHAjaf/rXGqTYgQzZT6pPOTndWeyDn/lTiKeNgaTCaxaW8/Gy5b4nXScjllI5MZ23pXfRZyylbHA/Y9MM1Gsw+xjb57V7IBPn1KplgEWyIQEbo+f/w==";
+
+// var ftpString = "U2FsdGVkX1+jcFED/CJbcYNiOJ42eBsjlxqmrcKWSIPH9Sao/4535zPQX5Fa7VYGAHSfkKCXbDpiUfJhkRRijaerS1lJ/k+dSfqsfl45ICkzMTJ7fBNVDj/242ur9ZG4HZDhSe1O/J4vEUboWDRBhg==";
 
 var uploadingFolder = "pinkpantherworld";
 var password = "";
