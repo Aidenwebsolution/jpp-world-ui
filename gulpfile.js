0ab2561680e0a0f82b5837a6023e6c2dece11eb4
gadarohan17 <gadarohan17@gmail.com>
2016-06-27 18:22:11 +0530

    panther world folder name change

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ var ftpString = "U2FsdGVkX19fEFj78reyTrJyfkOEEctph92umHAjaf/rXGqTYgQzZT6pPOTndWe
 
 // var ftpString = "U2FsdGVkX1+jcFED/CJbcYNiOJ42eBsjlxqmrcKWSIPH9Sao/4535zPQX5Fa7VYGAHSfkKCXbDpiUfJhkRRijaerS1lJ/k+dSfqsfl45ICkzMTJ7fBNVDj/242ur9ZG4HZDhSe1O/J4vEUboWDRBhg==";
 
-var uploadingFolder = "pinkpantherworld";
+var uploadingFolder = "pantherworld";
 var password = "";
 
 
